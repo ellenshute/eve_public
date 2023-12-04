03009dd0d5d93a3db1665366d57497caa6411bca
Ellen Shute <ef.shute@gmail>
2023-12-04 23:35:25 +0100

    2d latent space try

--- Step1_train_VAE.sh
+++ Step1_train_VAE.sh
@@ -1,8 +1,8 @@
 export MSA_data_folder='./data/MSA'
-export MSA_list='./data/mappings/cbbm_mapping.csv'
+export MSA_list='./data/mappings/cbbm_mapping_2D.csv'
 export MSA_weights_location='./data/weights'
 export VAE_checkpoint_location='./results/VAE_parameters'
-export model_name_suffix='CbbM(I)_Ute_MSA'
+export model_name_suffix='CbbM(I)_Ute_MSA_2D'
 export model_parameters_location='./EVE/default_model_params.json'
 export training_logs_location='./logs/'
 export protein_index=0
