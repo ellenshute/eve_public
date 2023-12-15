317ad3acc16bea6b6a178b690af9b0b3657383f0
Ellen Shute <ef.shute@gmail>
2023-12-15 10:44:50 +0100

    sample size change

--- Step3_latent_space.sh
+++ Step3_latent_space.sh
@@ -9,7 +9,7 @@ export training_logs_location='./logs/'
 export protein_index=0
 
 export output_latent_space_location='./results/evol_indices'
-export num_samples_latent_space=20000
+export num_samples_latent_space=20480
 export batch_size=2048
 
 python latent_space_visualisation.py \
